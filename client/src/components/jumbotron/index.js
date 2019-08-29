65414b1d8f47d1b4744b26376a781867d782950a
Aida Yrysbekova <aidiko20@gmail.com>
2019-08-29 11:30:12 -0400

    added all components

---
+++ client/src/components/jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron({ children }) {
+    return (
+        <div className="jumbotron">
+        <h1 className="display-4">(React) Google Books Search</h1>
+        <p className="lead">Search for and Save Books of interest</p>
+      </div>
+
+    )
+}
+export default Jumbotron;
---
+++ client/src/components/navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css"
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand" href="/">
+          Google Books
+        </a>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
+    
