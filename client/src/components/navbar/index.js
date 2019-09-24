3fe7cacb559e836bb9a42d6568bd05e47b6f0acb
Aida Yrysbekova <aidiko20@gmail.com>
2019-09-24 13:59:13 -0400

    fixed navbar and added list component

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,15 +1,76 @@
-import React from "react";
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
 import "./style.css"
 
-function Navbar() {
+class Navbar extends Component {
+  state= {
+    open:false,
+    width: window.innerWidth
+  };
+
+  updateWidth = () => {
+    const newState = {width: window.innerWidth};
+    if (this.state.open && newState.width > 991) {
+      newState.open = false;
+    }
+    this.setState(newState)
+  };
+
+  toggleNav = () => {
+    this.setState ({ open: !this.state.open });
+  };
+
+  componentDidMount() {
+    window.removeEventListener("resize", this.updateWidth)
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWidth)
+  }
+
+  render() {
     return (
-        <nav className="navbar navbar-expand-lg">
-        <a className="navbar-brand" href="/">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
+        <Link className="navbar-brand" to="/">
           Google Books
-        </a>
+        </Link>
+        <button
+        onClick={this.toggleNav}
+        className="navbar-toggler"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toogle navigation"
+        >
+          <span className="navbar-toggle-icon" />
+        </button>
+        <div className={`${this.state.open ? "" : "collapse "} navbar-collapse`} id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+              onClick={this.toggleNav}
+              className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}
+              to="/"
+              >
+                Search
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+              onClick={this.toggleNav}
+              className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+              to="/saved"
+              >
+                Saved
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
-  
-  export default Navbar;
+}
+export default Navbar;
+
+
     
