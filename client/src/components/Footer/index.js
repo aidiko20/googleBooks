8bef6b990f78e48cb014f3817e146ecbfdaa17ac
Aida Yrysbekova <aidiko20@gmail.com>
2019-09-24 11:33:59 -0400

    added footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <hr />
+            <p className="pull-right">
+                <i className="fab fa-github" /> Bulit using React.js By Aida Yrysbekova
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
